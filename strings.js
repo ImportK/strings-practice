7b62dfdf910623370c62b000499665116aec7f3d
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-14 21:16:35 +0800

    Initial commit

--- strings.js
+++ strings.js
@@ -19,7 +19,7 @@ console.log("Favorite Actor First Name:" + ' ' + favActorFirstName);
 console.log("Favorite Actor Last Name:" + ' ' + favActorLastName);
 console.log("Full Name:" + ' ' + fullName);
 console.log("Uppercase Full Name:" + ' ' + uppercase);
-console.log("Lowercase Pet Name:" + ' ' + lowercase);
+console.log("Lowercase Pet Name:" + ' ' + lowercase)
 console.log("Message:" + ' ' + message);
 
 
