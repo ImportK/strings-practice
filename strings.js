481b23d85011f5972140daf69c787033bcd74587
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-14 21:14:30 +0800

    Initial commit

---
+++ strings.js
@@ -0,0 +1,22 @@
+// Store variables.
+let favActorFirstName = "Dennis";
+let favActorLastName = "Trillo";
+let fullName = favActorFirstName + ' ' + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let petName = "chewy"
+let lowercase = petName.toUpperCase();
+
+// Append the message.
+let message = "My favorite actor is " + fullName.toUpperCase();
+message += ', ' + "his best show is Maria Clara at Ibarra.";
+
+// Log all variables to the console.
+console.log("Favorite Actor First Name:" + ' ' + favActorFirstName);
+console.log("Favorite Actor Last Name:" + ' ' + favActorLastName);
+console.log("Full Name:" + ' ' + fullName);
+console.log("Lowercase Full Name:" + ' ' + uppercase);
+console.log("Uppercase Pet Name:" + ' ' + lowercase);
+console.log("Message:" + ' ' + message);
+
+
