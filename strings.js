f05fc8e9b5d07eacb449045b2022c6e5ab7f0868
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-14 21:14:59 +0800

    Initial commit

--- strings.js
+++ strings.js
@@ -1,22 +1,25 @@
-// Store variables.
+// Declare variables.
 let favActorFirstName = "Dennis";
 let favActorLastName = "Trillo";
+
+// Concatenate the first and last name into variable called fullName.
 let fullName = favActorFirstName + ' ' + favActorLastName;
 
-let uppercase = fullName.toLowerCase();
-let petName = "chewy"
-let lowercase = petName.toUpperCase();
+// Changing case.
+let uppercase = fullName.toUpperCase();
+let petName = "CHEWY"
+let lowercase = petName.toLowerCase();
 
 // Append the message.
 let message = "My favorite actor is " + fullName.toUpperCase();
-message += ', ' + "his best show is Maria Clara at Ibarra.";
+message += ',' + ' his best show is Maria Clara at Ibarra. ';
 
 // Log all variables to the console.
 console.log("Favorite Actor First Name:" + ' ' + favActorFirstName);
 console.log("Favorite Actor Last Name:" + ' ' + favActorLastName);
 console.log("Full Name:" + ' ' + fullName);
-console.log("Lowercase Full Name:" + ' ' + uppercase);
-console.log("Uppercase Pet Name:" + ' ' + lowercase);
+console.log("Uppercase Full Name:" + ' ' + uppercase);
+console.log("Lowercase Pet Name:" + ' ' + lowercase);
 console.log("Message:" + ' ' + message);
 
 
